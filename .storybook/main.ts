961e9d0081990acc1b351fdcc381d531e08ce673
oahmaro <osamaahmaro@gmail.com>
2024-01-10 09:16:49 +0200

    Update storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,15 +2,21 @@ import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
   stories: ['../components/**/*.(stories|story).@(js|jsx|ts|tsx)'],
+
   addons: [
     '@storybook/addon-essentials',
     'storybook-dark-mode',
     '@storybook/addon-styling-webpack',
   ],
+
   framework: {
     name: '@storybook/nextjs',
     options: {},
   },
+
+  docs: {
+    autodocs: true
+  }
 };
 
 export default config;
