f59ff1b607924406a3075d8487b1cac139705116
oahmaro <osamaahmaro@gmail.com>
2024-01-27 18:03:16 +0200

    Enhance UI

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -3,10 +3,10 @@ import { StatCardList } from './_components';
 
 export default async function HomePage() {
   return (
-    <Stack maw={700} m="0 auto">
+    <Stack m="0 auto">
       <Title order={1}>ברוך הבא 👋</Title>
 
-      <Stack gap={0} align="center">
+      <Stack gap={0}>
         <Text c="dimmed">
           אפליקציה זו פותרת את ניהול, יצירה ומעקב הזמנות בקלות, ומציעה יכולת להדפיסן בקלות. פשוט את
           התהליכים שלך ושמור על סדר בצורה יעילה.
