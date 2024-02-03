08e735107718237eaa34f9428c084a68f6c10263
oahmaro <osamaahmaro@gmail.com>
2024-02-03 11:16:09 +0200

    Remove table-loading component

--- components/index.ts
+++ components/index.ts
@@ -3,6 +3,5 @@ export * from './orders-table';
 export * from './table-footer';
 export * from './static-field';
 export * from './main-drawer';
-export * from './table-loading';
 export * from './link';
 export * from './nprogress-complete';
--- components/table-loading/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as TableLoading } from './table-loading';
