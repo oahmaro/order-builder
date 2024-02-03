ae6705869d37038204436f248da3a510effcbfda
oahmaro <osamaahmaro@gmail.com>
2024-02-03 11:00:17 +0200

    Update Storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from '@storybook/nextjs';
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
+import path from 'path';
 
 const config: StorybookConfig = {
   stories: ['../components/**/*.(stories|story).@(js|jsx|ts|tsx)'],
@@ -8,6 +10,18 @@ const config: StorybookConfig = {
     'storybook-dark-mode',
     '@storybook/addon-styling-webpack',
   ],
+
+  webpackFinal: async (config) => {
+    config.resolve = config.resolve || {};
+
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, '../tsconfig.json'),
+      }),
+    ];
+    return config;
+  },
 
   framework: {
     name: '@storybook/nextjs',
