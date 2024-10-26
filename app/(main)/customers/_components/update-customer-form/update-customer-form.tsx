ee360dd4eeaae50dcc61e9626ec8f59095554e99
oahmaro <osamaahmaro@gmail.com>
2024-10-26 17:35:03 +0300

    Reset form after save

--- app/(main)/customers/_components/update-customer-form/update-customer-form.tsx
+++ app/(main)/customers/_components/update-customer-form/update-customer-form.tsx
@@ -77,6 +77,7 @@ export default function UpdateCustomerForm({
           message: response.message,
           color: 'green',
         });
+        form.resetDirty();
       } else {
         notifications.show({
           title: 'Error',
